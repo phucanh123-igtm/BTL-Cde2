aab7aa212f76b01307b4039b54e413ee1da07547
phucanh123-igtm <phucanhta986@gmail.com>
2026-05-23 21:06:04 +0700

    update code

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
@@ -8,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.btl"
+    namespace = "com.educode.btl"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -23,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.btl"
+        applicationId = "com.educode.btl"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/educode/btl/MainActivity.kt
+++ android/app/src/main/kotlin/com/educode/btl/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.btl
+package com.educode.btl
 
 import io.flutter.embedding.android.FlutterActivity
 
